46089aca2b351aa0290177658babb4185f40cfe2
Carlos Duran <cosayco@gmail.com>
2024-09-14 20:55:55 -0300

    evaluacion sesion 6

--- sesion6/eva/assets/js/scripts.js
+++ sesion6/eva/assets/js/scripts.js
@@ -9,9 +9,50 @@
 • Debes utilizar métodos propios de los arreglos como “push”, “filter” o “length” para manipular la lista de tareas.
 */
 
-
-var tarea = {
-    descripcion: this.descripcion,
-    estado: this.estado
-
+class Tarea {
+    constructor(descripcion, estado) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
 }
+
+const listaTareas = [];
+
+function agregaTarea(descripcion) {
+    const nuevaTarea = new Tarea(descripcion, "pendiente");
+    listaTareas.push(nuevaTarea);
+    return `Se agrego la tarea "${descripcion}".`
+}
+
+function marcaTareaCompletada(descripcion) {
+    const completa = listaTareas.find(tarea => tarea.descripcion === descripcion);
+    if (completa) {
+        completa.estado = "completada";
+        return `La tarea "${descripcion}" ha sido marcada como completada.`;
+    } else {
+        return `No se encontró ninguna tarea con la descripción "${descripcion}".`;
+    }
+}
+
+function filtraTarea(estado) {
+    const filtradas = listaTareas.filter(tarea => tarea.estado === estado);
+    const descripcion = filtradas.map(tarea => tarea.descripcion);
+    return descripcion;
+}
+
+function totalTareas() {
+    return listaTareas.length;
+}
+
+// Validaciones
+console.log(agregaTarea("Comprar leche"));
+console.log(agregaTarea("Hacer ejercicio"));
+console.log(agregaTarea("Preparar desayuno"));
+console.log(agregaTarea("Asistir a clases"));
+console.log(agregaTarea("Aprender Javascript"));
+
+console.log(marcaTareaCompletada("Preparar desayuno"));
+console.log(marcaTareaCompletada("Asistir"));
+
+console.log("Tareas pendientes:", filtraTarea("pendiente"));
+console.log("Total de tareas: " + totalTareas());
